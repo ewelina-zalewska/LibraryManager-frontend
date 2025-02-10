0dc5bd139ee648500868610afd5e427d7279c344
Ewelina <ewe.zale@gmail.com>
2025-02-10 18:19:35 +0100

    feat: create main container

---
+++ src/components/CollapsibleAccordion.tsx
@@ -0,0 +1,22 @@
+﻿import { ReactNode } from "react";
+import { NavBar } from "@/components/navigation/Navbar";
+import { LinkToPage } from "@/components/navigation/LinkToPage";
+
+type CollapsibleAccordionProps = {
+	children?: ReactNode;
+};
+
+export const CollapsibleAccordion = ({
+	children,
+}: CollapsibleAccordionProps) => {
+	return (
+		<>
+			<NavBar />
+			<main className="md:w-full w-screen overflow-y-auto overflow-x-hidden px-main-x py-main-y h-main-small md:h-main flex flex-col gap-main">
+				<section className="flex-grow">{children}</section>
+				<LinkToPage link="/" title="Go Back" />
+			</main>
+			<footer className="hidden md:block h-footer"></footer>
+		</>
+	);
+};
