68b0268377d118d6f401c77745af8e23f41439df
Ewelina <ewe.zale@gmail.com>
2025-02-10 18:17:37 +0100

    feat: create button component

---
+++ src/components/navigation/ChangePageButton.tsx
@@ -0,0 +1,30 @@
+﻿import { MouseEventHandler } from "react";
+
+type ChangePageButton = {
+	disabled?: boolean;
+	btnLabel: string | number;
+	onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const ChangePageButton = ({
+	disabled,
+	btnLabel,
+	onClick,
+}: ChangePageButton) => {
+	const backgroundColor = !disabled
+		? "bg-theme-light-gray-color"
+		: "bg-radial-[at_1%_95%] from-orange-500 to-bold-900 to-65%";
+	const textColor = !disabled
+		? "text-theme-dark-gray-color"
+		: "text-theme-dark-color";
+
+	return (
+		<button
+			disabled={disabled}
+			onClick={onClick}
+			className={`py-2 px-4 rounded-lg hover:font-bold ${backgroundColor} ${textColor} cursor-pointer  `}
+		>
+			{btnLabel}
+		</button>
+	);
+};
---
+++ src/components/navigation/TheButton.tsx
@@ -0,0 +1,37 @@
+﻿import { MouseEventHandler } from "react";
+
+type ButtonProps = {
+	type?: "submit" | "reset" | "button";
+	disabled?: boolean;
+	btnLabel: string;
+	onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const TheButton = ({
+	type,
+	disabled,
+	btnLabel,
+	onClick,
+}: ButtonProps) => {
+	const backgroundColor = disabled
+		? "bg-theme-light-gray-color"
+		: "bg-radial-[at_1%_95%] from-orange-500 to-bold-900 to-65%";
+	const textColor = disabled
+		? "text-theme-dark-gray-color"
+		: "text-theme-dark-color";
+	const shadowColor = disabled
+		? "shadow-disabledElement"
+		: "shadow-lightBorder";
+
+	return (
+		<button
+			color="transparent"
+			type={type}
+			disabled={disabled}
+			onClick={onClick}
+			className={`py-2 px-4 rounded-xl hover:font-bold ${backgroundColor} ${textColor} ${shadowColor} cursor-pointer  `}
+		>
+			{btnLabel}
+		</button>
+	);
+};
