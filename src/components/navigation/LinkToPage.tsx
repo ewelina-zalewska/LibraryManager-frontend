ebe64ead4ea6e4a978faab7c43b40d15925d9d8a
Ewelina <ewe.zale@gmail.com>
2025-02-10 18:29:37 +0100

    feat: create link component

---
+++ src/components/navigation/LinkToPage.tsx
@@ -0,0 +1,23 @@
+﻿import { Link } from "@tanstack/react-router";
+import leftArrow from "@/assets/images/light_left_arrow_btn.png";
+
+type LinkToPageProps = {
+	title: string;
+	link: string;
+};
+
+export const LinkToPage = ({ title, link }: LinkToPageProps) => {
+	return (
+		<Link
+			to={link}
+			className=" w-fit p-2 mx-auto lg:mx-1 hover:font-bold flex gap-2 self-end place-self-end"
+		>
+			<img
+				src={leftArrow}
+				className="w-[30px] h-5 self-center "
+				alt="left arrow"
+			/>
+			<p>{title}</p>
+		</Link>
+	);
+};
