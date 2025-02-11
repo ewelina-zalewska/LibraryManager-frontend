481a4a420995b8530e36b1bab5cf2ebabc0667a3
Ewelina <ewe.zale@gmail.com>
2025-02-11 17:01:44 +0100

    feat: create errors component

---
+++ src/components/handleData/FieldErrors.tsx
@@ -0,0 +1,12 @@
+﻿import { FieldErrorsProps } from "@/types";
+
+export const FieldErrors = ({ errors }: FieldErrorsProps) => {
+	if (!errors.length) return null;
+	return (
+		<ul className="text-error text-center">
+			{errors.map((error, index) => (
+				<li key={index}>{error}</li>
+			))}
+		</ul>
+	);
+};
