9abf40db11e23e912414bb22adf089dced348e50
Ewelina <ewe.zale@gmail.com>
2025-02-11 09:45:18 +0100

    feat: build a layout with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { routeTree } from "@/routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+const router = createRouter({
+	routeTree,
+	defaultNotFoundComponent: () => <p>Page not found</p>,
+	context: {
+		queryClient: undefined!,
+	},
+});
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const queryClient = new QueryClient();
+
 export const App = () => {
-	return <h1>Library Manager</h1>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} context={{ queryClient }} />
+		</QueryClientProvider>
+	);
 };
---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+﻿import React, { Suspense } from "react";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+
+const TanStackRouterDevtools = import.meta.env.DEV
+	? React.lazy(() =>
+			import("@tanstack/router-devtools").then((res) => ({
+				default: res.TanStackRouterDevtools,
+				defaultStaleTime: 5000,
+			})),
+		)
+	: () => null;
+
+const RootComponent = () => {
+	return (
+		<>
+			<Outlet />
+			<Suspense>
+				<TanStackRouterDevtools />
+			</Suspense>
+		</>
+	);
+};
+
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
+	component: RootComponent,
+});
