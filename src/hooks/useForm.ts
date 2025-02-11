fb2957691fd68aaf1e97130162c48c7b2ae4b2e6
Ewelina <ewe.zale@gmail.com>
2025-02-11 16:58:58 +0100

    create useForm hook

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,26 @@
+﻿import { useState } from "react";
+import { FormChangeEvent } from "@/types";
+
+type UseFormReturn<T> = [
+	T,
+	React.Dispatch<React.SetStateAction<T>>,
+	(e: FormChangeEvent) => void,
+];
+
+export const useForm = <T>(initialValue: T): UseFormReturn<T> => {
+	const [formState, setFormState] = useState<T>(initialValue);
+
+	const getValue = (target: FormChangeEvent["target"]) => {
+		if (target.type === "number") return Number(target.value);
+		return target.value;
+	};
+
+	const HANDLE_CHANGE = (e: FormChangeEvent) => {
+		setFormState((prevFormState) => ({
+			...prevFormState,
+			[e.target.name]: getValue(e.target),
+		}));
+	};
+
+	return [formState, setFormState, HANDLE_CHANGE];
+};
