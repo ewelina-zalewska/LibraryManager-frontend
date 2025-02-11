df156234606a32fadd9c24bab49c17cf2443f1d7
Ewelina <ewe.zale@gmail.com>
2025-02-11 16:53:48 +0100

    feat: create homepage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+﻿import { NavBar } from "@/components/navigation/Navbar";
+
+export const HomePage = () => {
+	return <NavBar />;
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+﻿import { createFileRoute } from "@tanstack/react-router";
+import { HomePage } from "@/pages/HomePage";
+import { DataLoading } from "@/components/handleData/DataLoading";
+import { TheError } from "@/components/handleData/TheError";
+import { PageNotFound } from "@/components/handleData/PageNotFound";
+
+export const Route = createFileRoute("/")({
+	component: HomePage,
+	notFoundComponent: () => PageNotFound("Page"),
+	pendingComponent: DataLoading,
+	errorComponent: TheError,
+});
