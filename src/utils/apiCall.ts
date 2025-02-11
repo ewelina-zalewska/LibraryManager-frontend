d7f06a56d6aa368dba694c2c678e6d8d6e2b33e4
Ewelina <ewe.zale@gmail.com>
2025-02-11 09:44:01 +0100

    create api call function

---
+++ src/utils/apiCall.ts
@@ -0,0 +1,19 @@
+﻿type ApiConfig<P = void> = {
+	id?: string;
+	method?: "GET" | "POST" | "DELETE" | "PATCH" | "PUT";
+	body?: P;
+};
+
+export const apiCall = async <R, P = void>(
+	url: string,
+	config?: ApiConfig<P>,
+) => {
+	const API_BASE = import.meta.env.VITE_API_URL;
+
+	const response = await fetch(`${API_BASE}${url}`, {
+		method: config?.method || "GET",
+		body: config?.body ? JSON.stringify(config.body) : undefined,
+	});
+	const data = await response.json();
+	return data as Promise<R>;
+};
