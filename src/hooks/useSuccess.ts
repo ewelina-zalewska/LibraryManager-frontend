0f6f385f2d64a93d792e5bdc86424050dcbbf617
Ewelina <ewe.zale@gmail.com>
2025-02-11 16:59:59 +0100

    create useSuccess hook

---
+++ src/hooks/useSuccess.ts
@@ -0,0 +1,16 @@
+﻿import { useEffect, useState } from "react";
+
+export const useSuccess = () => {
+	const [success, setSuccess] = useState<boolean>(false);
+	useEffect(() => {
+		if (!success) return;
+		const timeout = setTimeout(() => {
+			setSuccess(false);
+		}, 3000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [success]);
+
+	return { success, setSuccess };
+};
