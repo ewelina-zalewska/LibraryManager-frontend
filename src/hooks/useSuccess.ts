18f1de5a6b13b0f9ac97b683edeb30438a01c63e
Ewelina <ewe.zale@gmail.com>
2025-02-17 10:59:20 +0100

    navigate to a specific path

--- src/hooks/useSuccess.ts
+++ src/hooks/useSuccess.ts
@@ -1,16 +1,22 @@
-﻿import { useEffect, useState } from "react";
+﻿import { ResponseMessage } from "@/types";
+import { useNavigate } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
-export const useSuccess = () => {
+export const useSuccess = (page: string, response?: ResponseMessage) => {
 	const [success, setSuccess] = useState<boolean>(false);
+	const navigate = useNavigate();
+
 	useEffect(() => {
 		if (!success) return;
 		const timeout = setTimeout(() => {
 			setSuccess(false);
+			if (response?.status === "success") navigate({ to: page });
+			console.log(response?.status);
 		}, 3000);
 		return () => {
 			clearTimeout(timeout);
 		};
-	}, [success]);
+	}, [success, response]);
 
 	return { success, setSuccess };
 };
