f4cb9d580eeb3dbc5bef4553f457d9d749a7a088
Ewelina <ewe.zale@gmail.com>
2025-02-17 11:02:19 +0100

    create useDisabled button hook

---
+++ src/hooks/useDisabled.ts
@@ -0,0 +1,16 @@
+﻿import { useEffect, useState } from "react";
+
+export const useDisabled = () => {
+	const [isDisabled, setDisabled] = useState<boolean>(false);
+	useEffect(() => {
+		if (!isDisabled) return;
+		const timeout = setTimeout(() => {
+			setDisabled(false);
+		}, 3000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [isDisabled]);
+
+	return { isDisabled, setDisabled };
+};
