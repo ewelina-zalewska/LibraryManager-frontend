c3bf498305b5e12fe8dd9eff8aad86bb04838a4f
Ewelina <ewe.zale@gmail.com>
2025-02-22 14:24:13 +0100

    add get time fn

---
+++ src/utils/getTime.ts
@@ -0,0 +1,8 @@
+﻿export const getTime = () => {
+	const date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const getTime = (time: number) => (time < 10 ? `0${time}` : time);
+
+	return `${getTime(hours)}:${getTime(minutes)}`;
+};
