45014bee574e23915d39ea32541c484bc6b9e171
Ewelina <ewe.zale@gmail.com>
2025-02-22 14:57:49 +0100

    create logout hook

---
+++ src/mutations/useLogoutMutation.ts
@@ -0,0 +1,16 @@
+﻿import { useMutation } from "@tanstack/react-query";
+import { ResponseMessage } from "@/types";
+import { useApi } from "@/hooks/useApi";
+
+type LogoutDto = { message: "Logout" };
+
+export const useLogoutMutation = () => {
+	const { API_POST } = useApi();
+
+	const { mutate, error, isPending, data } = useMutation({
+		mutationKey: ["logout"],
+		mutationFn: async (body: LogoutDto) =>
+			API_POST<ResponseMessage, LogoutDto>("logout", body),
+	});
+	return { mutate, error, isPending, data };
+};
