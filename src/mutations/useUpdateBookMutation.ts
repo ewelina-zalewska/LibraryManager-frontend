4bba40907a6fc4b7da7a35c528bf528cf47084aa
Ewelina <ewe.zale@gmail.com>
2025-02-22 14:57:17 +0100

    create book update hook

---
+++ src/mutations/useUpdateBookMutation.ts
@@ -0,0 +1,23 @@
+﻿import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BookDto, ResponseMessage } from "@/types";
+import { useApi } from "@/hooks/useApi";
+
+export const useUpdateBookMutation = (id: string) => {
+	const { API_PATCH } = useApi();
+	const queryClient = useQueryClient();
+
+	const { mutate, data, error, isPending } = useMutation({
+		mutationKey: ["books", id],
+		mutationFn: async (body: BookDto) => {
+			return API_PATCH<ResponseMessage, BookDto>(`books/${id}`, body);
+		},
+
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ["books"],
+			});
+		},
+	});
+
+	return { mutate, error, data, isPending };
+};
