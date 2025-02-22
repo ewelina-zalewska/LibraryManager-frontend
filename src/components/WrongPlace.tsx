cf1cc9ea09771b534de3264f8dd4a8fd8737e920
Ewelina <ewe.zale@gmail.com>
2025-02-22 14:27:35 +0100

    feat: create wrong place component

---
+++ src/components/WrongPlace.tsx
@@ -0,0 +1,27 @@
+﻿import { useEffect } from "react";
+import { Outlet, useNavigate } from "@tanstack/react-router";
+
+export const WrongPlace = () => {
+	const time: number = 3000;
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			navigate({ to: "/" });
+		}, time);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [navigate]);
+
+	return (
+		<div className="absolute top-0 left-0 w-full h-screen bg-theme-translucent-color">
+			<h3 className="text-[30px] text-theme-error-color text-center mt-[40vh]">
+				This is a wrong place. Go back to Home Page.
+			</h3>
+			<Outlet />
+		</div>
+	);
+};
---
+++ src/routes/$.tsx
@@ -0,0 +1,6 @@
+﻿import { WrongPlace } from "@/components/WrongPlace";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/$")({
+	component: WrongPlace,
+});
