3ad61397c560f57cb37771e79ef3566c937348dd
Ewelina <ewe.zale@gmail.com>
2025-02-23 11:30:48 +0100

    create get current page fn

---
+++ src/utils/getCurrentPageFromAdmin.ts
@@ -0,0 +1,8 @@
+﻿import { getRouteApi } from "@tanstack/react-router";
+
+const booksRoute = getRouteApi("/auth/_withAdminAuth/admin/books");
+
+export const getCurrentPageFromAdmin = () => {
+	const { page: currentPage } = booksRoute.useSearch();
+	return currentPage;
+};
---
+++ src/utils/getCurrentPageFromUser.ts
@@ -0,0 +1,8 @@
+﻿import { getRouteApi } from "@tanstack/react-router";
+
+const booksRoute = getRouteApi("/auth/_withUserAuth/user/books");
+
+export const getCurrentPageFromUser = () => {
+	const { page: currentPage } = booksRoute.useSearch();
+	return currentPage;
+};
